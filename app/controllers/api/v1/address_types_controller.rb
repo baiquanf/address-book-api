438c3aca391e97dc89eeaab3729442434d6e2ab2
Baiquan Fan <baiquanf.dev@gmail.com>
2020-06-04 21:11:47 +0300

    added Read API for AddressType (Create and Update through seeds).

--- app/controllers/api/v1/address_types_controller.rb
+++ app/controllers/api/v1/address_types_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AddressTypesController < ApplicationController
+  respond_to :json
+
+  def index
+    address_types = AddressType.all.page(params[:page]).per(params[:per_page])
+    render json: address_types, meta: pagination(address_types, params[:per_page])
+  end
+
+  def show
+    respond_with AddressType.find(params[:id])
+  end
 end
---
+++ spec/controllers/api/v1/address_types_controller_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::AddressTypesController, type: :controller do
+  describe "GET #index" do
+    before(:each) do
+      4.times { FactoryBot.create :address_type }
+      get :index
+    end
+
+    it "returns the people object into each person" do
+      json_response[:address_types].each do |address_type|
+        expect(address_type).to be_present
+      end
+    end
+
+    it "returns 4 records from the database" do
+      expect(json_response[:address_types].length).to eq(4)
+    end
+
+    it { is_expected.to respond_with 200 }
+
+    context "when is not receiving any address_type_ids parameter" do
+      before(:each) do
+        get :index
+      end
+
+      it "returns 4 records from the database" do
+        expect(json_response[:address_types].length).to eq(4)
+      end
+
+      it "returns the people object into each person" do
+        json_response[:address_types].each do |address_type|
+          expect(address_type[:name]).to be_present
+        end
+      end
+
+      it_behaves_like "paginated list"
+
+      it { is_expected.to respond_with 200 }
+    end
+  end
+
+  describe "GET #show" do
+    before(:each) do
+      @address_type = FactoryBot.create :address_type
+      get :show, params: { id: @address_type.id }
+    end
+
+    it "has the attributes as a embeded object" do
+      expect(json_response[:name]).to eql @address_type.name
+    end
+
+    it { is_expected.to respond_with 200 }
+  end
+end
