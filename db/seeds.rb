608b2d8d768bb28f8b64e1a9a1dd6103f2f4d4f7
Baiquan Fan <baiquanf.dev@gmail.com>
2020-06-05 11:00:18 +0300

    added seeds to address_type table as address type id should be fixed for addresses table's reference.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AddressType.delete_all
+AddressType.create(id: 1, name: 'Home')
+AddressType.create(id: 2, name: 'Work')
+AddressType.create(id: 3, name: 'Cottage')
+AddressType.create(id: 4, name: 'Other')
